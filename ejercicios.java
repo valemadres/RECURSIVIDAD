b361f40996e724d8903b88332273a3ad45400704
Noel Vallejos <noel.72712510.va@gmail.com>
2022-05-30 08:58:17 -0400

    recursividad y factorial

---
+++ ejercicios.java
@@ -0,0 +1,11 @@
+public class ejercicios{
+    public static int factorial (int n){
+        int factorial;
+        if (n ==1 ){
+            factorial=n;
+        }else{
+            factorial = n *factorial (n:n -1);
+            
+        return factorial;
+    }
+}
---
+++ main.java
@@ -0,0 +1,5 @@
+public class main{
+    public static void main(String[] args){args:{}
+        System.out.println(ejercicios.factorial(n:5));
+    }
+}
