f8f7b89fe940d582e310f4c00696378a08964167
Noel_Vallejos_Ugarte <101579402+valemadres@users.noreply.github.com>
2022-06-20 10:40:41 -0400

    Update main.java

--- main.java
+++ main.java
@@ -2,6 +2,9 @@ public class main{
     public static void main(String[] args) {
         //System.out.println(ejercicios.factorial(n:5));
 
-        System.out.println(Ejercicio2.MCD(190, 200));
+        //System.out.println(Ejercicio2.MCD(190, 200));
+        
+        int vector[]={2,3,9,7,4,1};
+        System.out.println(Ejercicio_3.sumaVector(vector,5));
     }
 }
